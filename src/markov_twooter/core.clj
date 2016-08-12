396279969804f5608a0590a6f7b1d0a9920f8846
Jones Melton <shavah@gmail.com>
2016-08-12 10:14:24 -0500

    Add basic chains and spec

--- src/markov_twooter/core.clj
+++ src/markov_twooter/core.clj
@@ -1,7 +1,29 @@
 (ns markov-twooter.core
   (:gen-class))
 
+(defn word-chain [word-transitions]
+  (reduce (fn [ r t] (merge-with clojure.set/union r
+                                 (let [[a b c] t]
+                                   {[a b] (if c 
+                                            #{c} 
+                                            #{})})))
+          {}
+          word-transitions))
+
+(defn text->word-chain [s]
+  (let [words (clojure.string/split s #"[\s|\n]")
+        word-transitions (partition-all 3 1 words)]
+    (word-chain word-transitions)))
+
+(defn walk-chain [prefix chain result]
+  (let [suffixes (get chain prefix)]
+    (if (empty? suffixes)
+      result
+      (let [suffix (first (shuffle suffixes))
+            new-prefix [(last prefix) suffix]]
+        (recur new-prefix chain (conj result suffix))))))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Public interface for markov-generator."
   [& args]
-  (println "Hello, World!"))
+  (println "In Progress"))
--- test/markov_twooter/core_test.clj
+++ test/markov_twooter/core_test.clj
@@ -2,6 +2,61 @@
   (:require [clojure.test :refer :all]
             [markov-twooter.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest CI-proof
+  (testing "Always-passing test for CI verification."
+    (is (= 0 0))))
+
+
+(deftest test-word-chain
+  (testing "it produces a chain of the possible two step transitions between suffixes and prefixes"
+    (let [example '(("And" "the" "Golden")
+                    ("the" "Golden" "Grouse")
+                    ("And" "the" "Pobble")
+                    ("the" "Pobble" "who"))]
+      (is (= {["the" "Pobble"] #{"who"}
+              ["the" "Golden"] #{"Grouse"}
+              ["And" "the"] #{"Pobble" "Golden"}}
+             (word-chain example))))))
+
+(deftest test-text->word-chain
+  (testing "string with spaces and newlines"
+    (let [example "And the Golden Grouse\nAnd the Pobble who"]
+     (is (= {["who" nil] #{}
+             ["Pobble" "who"] #{}
+             ["the" "Pobble"] #{"who"}
+             ["Grouse" "And"] #{"the"}
+             ["Golden" "Grouse"] #{"And"}
+             ["the" "Golden"] #{"Grouse"}
+             ["And" "the"] #{"Pobble" "Golden"}}
+            (text->word-chain example))))))
+
+(deftest test-walk-chain
+  (let [chain {["who" nil] #{},
+               ["Pobble" "who"] #{},
+               ["the" "Pobble"] #{"who"},
+               ["Grouse" "And"] #{"the"},
+               ["Golden" "Grouse"] #{"And"},
+               ["the" "Golden"] #{"Grouse"},
+               ["And" "the"] #{"Pobble" "Golden"}}]
+    (testing "dead end"
+      (let [prefix ["the" "Pobble"]]
+        (is (= ["the" "Pobble" "who"]
+               (walk-chain prefix chain prefix)))))))
+
+(deftest test-walk-chain
+  (let [chain {["who" nil] #{},
+               ["Pobble" "who"] #{},
+               ["the" "Pobble"] #{"who"},
+               ["Grouse" "And"] #{"the"},
+               ["Golden" "Grouse"] #{"And"},
+               ["the" "Golden"] #{"Grouse"},
+               ["And" "the"] #{"Pobble" "Golden"}}]
+    (testing "dead end"
+      (let [prefix ["the" "Pobble"]]
+        (is (= ["the" "Pobble" "who"]
+               (walk-chain prefix chain prefix)))))
+    (testing "multiple choices"
+      (with-redefs [shuffle (fn [c] c)]
+        (let [prefix ["And" "the"]]
+          (is (= ["And" "the" "Pobble" "who"]
+                 (walk-chain prefix chain prefix))))))))
